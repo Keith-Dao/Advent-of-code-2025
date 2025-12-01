39ae4996d98ef23ee5c0af88b7b95ea2804d2b0e
Keith Dao <keith.dao.248@gmail.com>
2025-12-01 17:55:00 +1000

    refactor: extract common input parsing and fix linter warnings.

--- src/advent_of_code_2025/day_01/solver.py
+++ src/advent_of_code_2025/day_01/solver.py
@@ -1,7 +1,11 @@
 """Day 1 solver."""
 
-import pathlib
 import sys
+from typing import TYPE_CHECKING, override
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+    import pathlib
 
 from .. import base
 
@@ -9,44 +13,54 @@ from .. import base
 class Solver(base.Solver):
     """Day 1 solver."""
 
+    def _parse_input(
+        self, filepath: pathlib.Path
+    ) -> Generator[tuple[int, int], None, None]:
+        """Parses the input.
+
+        Args:
+            filepath: The input filepath.
+
+        Yields:
+            The amount to shift and the total number of full rotations respectively.
+        """
+        with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
+            for line in file:
+                negative = line[0] == "L"
+                amount = int(line[1:])
+
+                full_rotations = amount // 100
+                amount %= 100
+                if negative:
+                    amount = -amount
+
+                yield amount, full_rotations
+
+    @override
     def part_1(self, filepath: pathlib.Path) -> int | str:
         result = 0
         current = 50
 
-        with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            for line in file:
-                negative = line[0] == "L"
-                amount = int(line[1:])
-                if negative:
-                    amount = -amount
+        for amount, _ in self._parse_input(filepath):
+            current += amount
+            current %= 100
 
-                current += amount
-                current %= 100
-
-                result += current == 0
+            result += current == 0
 
         return result
 
+    @override
     def part_2(self, filepath: pathlib.Path) -> int | str:
         result = 0
         current = 50
 
-        with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            for line in file:
-                negative = line[0] == "L"
-                amount = int(line[1:])
+        for amount, full_rotations in self._parse_input(filepath):
+            result += full_rotations
+            result += current != 0 and (
+                current + amount <= 0 or current + amount >= 100
+            )
 
-                result += amount // 100
-                amount %= 100
-
-                if negative:
-                    amount = -amount
-
-                result += current != 0 and (
-                    current + amount <= 0 or current + amount >= 100
-                )
-
-                current += amount
-                current %= 100
+            current += amount
+            current %= 100
 
         return result
