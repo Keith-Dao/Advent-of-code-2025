7f16a49199dce03c6f5ed574763271270fdb74aa
Keith Dao <keith.dao.248@gmail.com>
2025-12-02 21:21:52 +1000

    feat: add initial day two solver.

--- src/advent_of_code_2025/day_02/solver.py
+++ src/advent_of_code_2025/day_02/solver.py
@@ -15,9 +15,34 @@ class Solver(base.Solver):
     @override
     def part_1(self, filepath: pathlib.Path) -> int | str:
         with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            pass
+            line = file.read()
+
+        result = 0
+        for ranges in line.split(","):
+            start, end = ranges.split("-")
+            for x in range(int(start), int(end) + 1):
+                s = str(x)
+                if len(s) % 2 == 1:
+                    continue
+
+                if s[: len(s) // 2] == s[len(s) // 2 :]:
+                    result += x
+
+        return result
 
     @override
     def part_2(self, filepath: pathlib.Path) -> int | str:
         with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            pass
+            line = file.read()
+
+        result = 0
+        for ranges in line.split(","):
+            start, end = ranges.split("-")
+            for x in range(int(start), int(end) + 1):
+                s = str(x)
+                for i in range(1, len(s) // 2 + 1):
+                    if all(s[:i] == s[j : j + i] for j in range(i, len(s), i)):
+                        result += x
+                        break
+
+        return result
