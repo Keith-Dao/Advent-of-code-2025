b09b4d69994f493b90f4e9d743b4af2fea732db4
Keith Dao <keith.dao.248@gmail.com>
2025-12-02 23:02:26 +1000

    feat: improve part 2 run speed.

--- src/advent_of_code_2025/day_02/solver.py
+++ src/advent_of_code_2025/day_02/solver.py
@@ -1,9 +1,11 @@
 """Day 2 solver."""
 
+import bisect
 import sys
 from typing import TYPE_CHECKING, override
 
 if TYPE_CHECKING:
+    from collections.abc import Generator
     import pathlib
 
 from .. import base
@@ -12,37 +14,106 @@ from .. import base
 class Solver(base.Solver):
     """Day 2 solver."""
 
+    def _parse_input(
+        self, filepath: pathlib.Path
+    ) -> Generator[tuple[int, int], None, None]:
+        """Parses the input.
+
+        Args:
+            filepath: The input filepath.
+
+        Yields:
+            The start and end of the range inclusively.
+        """
+        result = [0, 0]
+        i = 0
+        with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
+            char = file.read(1)
+            while char != "":
+                if char == ",":
+                    yield result[0], result[1]
+                    result = [0, 0]
+                    i = 0
+                elif char.isdigit():
+                    result[i] = result[i] * 10 + int(char)
+                else:
+                    i += 1
+
+                char = file.read(1)
+
+        yield result[0], result[1]
+
+    def _make_intervals(self, filepath: pathlib.Path) -> list[tuple[int, int]]:
+        """Makes the intervals array from the input.
+
+        Args:
+            filepath: The input filepath.
+
+        Returns:
+            The sorted invervals array.
+        """
+        return sorted(interval for interval in self._parse_input(filepath))
+
+    def _generate_invalid_nums(
+        self, max_num: int, repeat_once: bool = False
+    ) -> Generator[int, None, None]:
+        """Generates all invalid numbers up to and including the maximum number.
+
+        Args:
+            max_num: The maximum number in the input.
+            repeat_once: If true, only supply numbers that have a subsequence
+                that repeats once. If false, the subsequence can repeat multiple
+                times.
+
+        Yields:
+            A number that has a repeated subsequence.
+        """
+        used: set[int] = set()
+        start_num = 1
+        mask = start_num * 10 + 1
+        while mask * start_num <= max_num:  # Control subsequence length
+            while mask * start_num <= max_num:  # Control number of repeats
+                for sequence in range(start_num, start_num * 10):
+                    num = sequence * mask
+                    if num > max_num:
+                        break
+
+                    if num in used:
+                        continue
+
+                    used.add(num)
+                    yield num
+
+                if repeat_once:
+                    break
+
+                mask = mask * start_num * 10 + 1
+
+            start_num *= 10
+            mask = start_num * 10 + 1
+
     @override
     def part_1(self, filepath: pathlib.Path) -> int | str:
-        with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            line = file.read()
+        intervals = self._make_intervals(filepath)
+        max_num = intervals[-1][1]
 
         result = 0
-        for ranges in line.split(","):
-            start, end = ranges.split("-")
-            for x in range(int(start), int(end) + 1):
-                s = str(x)
-                if len(s) % 2 == 1:
-                    continue
-
-                if s[: len(s) // 2] == s[len(s) // 2 :]:
-                    result += x
+        for num in self._generate_invalid_nums(max_num, repeat_once=True):
+            i = bisect.bisect_left(intervals, num, key=lambda interval: interval[1])
+            if intervals[i][0] <= num <= intervals[i][1]:
+                result += num
 
         return result
 
     @override
     def part_2(self, filepath: pathlib.Path) -> int | str:
-        with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            line = file.read()
+        intervals = self._make_intervals(filepath)
+        max_num = intervals[-1][1]
 
         result = 0
-        for ranges in line.split(","):
-            start, end = ranges.split("-")
-            for x in range(int(start), int(end) + 1):
-                s = str(x)
-                for i in range(1, len(s) // 2 + 1):
-                    if all(s[:i] == s[j : j + i] for j in range(i, len(s), i)):
-                        result += x
-                        break
+        for num in self._generate_invalid_nums(max_num, repeat_once=False):
+            i = bisect.bisect_left(intervals, num, key=lambda interval: interval[1])
+            if intervals[i][0] <= num <= intervals[i][1]:
+                result += num
 
         return result
