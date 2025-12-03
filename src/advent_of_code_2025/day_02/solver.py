e23550d5be9ec19fe5b91f8b0d5d51b7263b77e6
Keith Dao <keith.dao.248@gmail.com>
2025-12-03 21:49:29 +1000

    refactor: add generic solution for both parts of day 2.

--- src/advent_of_code_2025/day_02/solver.py
+++ src/advent_of_code_2025/day_02/solver.py
@@ -92,28 +92,37 @@ class Solver(base.Solver):
             start_num *= 10
             mask = start_num * 10 + 1
 
-    @override
-    def part_1(self, filepath: pathlib.Path) -> int | str:
+    def _generic_solve(
+        self, filepath: pathlib.Path, repeat_subsequence_once: bool
+    ) -> int:
+        """Generic solver for both parts.
+
+        Args:
+            filepath: The input filepath.
+            repeat_once: If true, only check numbers that have a subsequence
+                that repeats once. If false, check numbers where the subsequence can repeat
+                multiple times.
+
+        Returns:
+            The solution to the problem.
+        """
         intervals = self._make_intervals(filepath)
         max_num = intervals[-1][1]
 
         result = 0
-        for num in self._generate_invalid_nums(max_num, repeat_once=True):
+        for num in self._generate_invalid_nums(
+            max_num, repeat_once=repeat_subsequence_once
+        ):
             i = bisect.bisect_left(intervals, num, key=lambda interval: interval[1])
             if intervals[i][0] <= num <= intervals[i][1]:
                 result += num
 
         return result
+
+    @override
+    def part_1(self, filepath: pathlib.Path) -> int | str:
+        return self._generic_solve(filepath, repeat_subsequence_once=True)
 
     @override
     def part_2(self, filepath: pathlib.Path) -> int | str:
-        intervals = self._make_intervals(filepath)
-        max_num = intervals[-1][1]
-
-        result = 0
-        for num in self._generate_invalid_nums(max_num, repeat_once=False):
-            i = bisect.bisect_left(intervals, num, key=lambda interval: interval[1])
-            if intervals[i][0] <= num <= intervals[i][1]:
-                result += num
-
-        return result
+        return self._generic_solve(filepath, repeat_subsequence_once=False)
