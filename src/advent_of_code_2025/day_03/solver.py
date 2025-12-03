fe48e249f5facba72fa7a7c220d9f25d33118e4f
Keith Dao <keith.dao.248@gmail.com>
2025-12-03 21:32:14 +1000

    feat: add initial day three solver.

--- src/advent_of_code_2025/day_03/solver.py
+++ src/advent_of_code_2025/day_03/solver.py
@@ -14,10 +14,38 @@ class Solver(base.Solver):
 
     @override
     def part_1(self, filepath: pathlib.Path) -> int | str:
+        result = 0
         with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            pass
+            for line in file:
+                best = past = 0
+                for c in line.strip():
+                    x = int(c)
+                    best = max(best, past * 10 + x)
+                    past = max(past, x)
+
+                result += best
+
+        return result
 
     @override
     def part_2(self, filepath: pathlib.Path) -> int | str:
+        result = 0
         with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            pass
+            for line in file:
+                stack: list[int] = []
+                line = line.strip()
+                n = len(line)
+                for i, c in enumerate(line):
+                    x = int(c)
+                    while stack and len(stack) + n - i > 12 and x > stack[-1]:
+                        _ = stack.pop()
+
+                    if len(stack) < 12:
+                        stack.append(x)
+
+                num = 0
+                for x in stack:
+                    num = num * 10 + x
+                result += num
+
+        return result
--- test/test_day_03.py
+++ test/test_day_03.py
@@ -10,4 +10,15 @@ from .base_test import BaseTests
 class TestDay3(BaseTests):
     """Tests the day 3 solver."""
 
-    cases = []
+    cases = [
+        (
+            textwrap.dedent("""\
+                987654321111111
+                811111111111119
+                234234234234278
+                818181911112111
+                """),
+            357,
+            3121910778619,
+        )
+    ]
