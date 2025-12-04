ce92c34dc128320a51a24707b84aee0a0bfcdb12
Keith Dao <keith.dao.248@gmail.com>
2025-12-04 17:05:50 +1000

    refactor: extract common logic from both solutions.

--- src/advent_of_code_2025/day_04/solver.py
+++ src/advent_of_code_2025/day_04/solver.py
@@ -5,6 +5,7 @@ import sys
 from typing import TYPE_CHECKING, override
 
 if TYPE_CHECKING:
+    from collections.abc import Generator
     import pathlib
 
 from .. import base
@@ -15,77 +16,82 @@ class Solver(base.Solver):
 
     ROLL: str = "@"
 
-    @override
-    def part_1(self, filepath: pathlib.Path) -> int | str:
+    def _get_next_positions(self, i: int, j: int) -> Generator[tuple[int, int]]:
+        """Generates the 8 neighboring positions (including diagonals) for a given cell.
+
+        Args:
+            i: The row index of the current cell.
+            j: The column index of the current cell.
+
+        Yields:
+            The neighboring cell's coordinates.
+        """
+        for d_i, d_j in itertools.product([-1, 0, 1], repeat=2):
+            if d_i == d_j == 0:
+                continue
+
+            yield i + d_i, j + d_j
+
+    def _get_surrounding_counts(self, filepath: pathlib.Path) -> list[list[int]]:
+        """Gets the number of surrounding rolls for each cell in the grid.
+
+        Args:
+            filepath: The input filepath.
+
+        Returns:
+            A grid matching the input file with the count of surrounding
+            rolls. If the cell was not a roll, the count will be -1.
+        """
         with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
             grid = [line.strip() for line in file]
 
-        m, n = len(grid), len(grid[0])
-        result = 0
+        m = len(grid)
+        n = len(grid[0])
+
+        counts = [[-1] * n for _ in range(m)]
         for i, row in enumerate(grid):
             for j, x in enumerate(row):
                 if x != self.ROLL:
                     continue
 
                 count = 0
-                for d_i, d_j in itertools.product([-1, 0, 1], repeat=2):
-                    if d_i == d_j == 0:
-                        continue
-
-                    n_i = i + d_i
-                    n_j = j + d_j
+                for n_i, n_j in self._get_next_positions(i, j):
                     if not 0 <= n_i < m or not 0 <= n_j < n:
                         continue
 
                     count += grid[n_i][n_j] == self.ROLL
-                    if count >= 4:
-                        break
 
-                result += count < 4
+                counts[i][j] = count
 
-        return result
+        return counts
+
+    @override
+    def part_1(self, filepath: pathlib.Path) -> int | str:
+        counts = self._get_surrounding_counts(filepath)
+        return sum(sum(x != -1 and x < 4 for x in row) for row in counts)
 
     @override
     def part_2(self, filepath: pathlib.Path) -> int | str:
-        with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            grid = [line.strip() for line in file]
+        counts = self._get_surrounding_counts(filepath)
 
-        m, n = len(grid), len(grid[0])
-        counts = [[-1] * n for _ in range(m)]
+        m = len(counts)
+        n = len(counts[0])
+
         stack: list[tuple[int, int]] = []
-        for i, row in enumerate(grid):
+        for i, row in enumerate(counts):
             for j, x in enumerate(row):
-                if x != self.ROLL:
+                if x == -1 or x >= 4:
                     continue
 
-                count = 0
-                for d_i, d_j in itertools.product([-1, 0, 1], repeat=2):
-                    if d_i == d_j == 0:
-                        continue
-
-                    n_i = i + d_i
-                    n_j = j + d_j
-                    if not 0 <= n_i < m or not 0 <= n_j < n:
-                        continue
-
-                    count += grid[n_i][n_j] == self.ROLL
-
-                if count < 4:
-                    stack.append((i, j))
-                else:
-                    counts[i][j] = count
+                counts[i][j] = -1
+                stack.append((i, j))
 
         result = 0
         while stack:
             result += 1
             i, j = stack.pop()
 
-            for d_i, d_j in itertools.product([-1, 0, 1], repeat=2):
-                if d_i == d_j == 0:
-                    continue
-
-                n_i = i + d_i
-                n_j = j + d_j
+            for n_i, n_j in self._get_next_positions(i, j):
                 if not 0 <= n_i < m or not 0 <= n_j < n or counts[n_i][n_j] < 4:
                     continue
 
