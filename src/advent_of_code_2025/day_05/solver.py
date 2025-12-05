f75c10f9e4509c2fd60b3e1344f293789659e7be
Keith Dao <keith.dao.248@gmail.com>
2025-12-05 18:58:35 +1000

    feat: add initial day five solver.

--- src/advent_of_code_2025/day_05/solver.py
+++ src/advent_of_code_2025/day_05/solver.py
@@ -4,6 +4,8 @@ import sys
 from typing import TYPE_CHECKING, override
 
 if TYPE_CHECKING:
+    from collections.abc import Generator
+    from io import TextIOWrapper
     import pathlib
 
 from .. import base
@@ -12,12 +14,49 @@ from .. import base
 class Solver(base.Solver):
     """Day 5 solver."""
 
+    def _get_intervals(self, file: TextIOWrapper) -> Generator[tuple[int, int]]:
+        """Yields the intervals in the input.
+
+        Args:
+            file: The opened input file.
+
+        Yields:
+            The start and end interval inclusive.
+        """
+        line = file.readline().strip()
+        while line != "":
+            start, end = (int(x) for x in line.split("-"))
+            yield start, end
+            line = file.readline().strip()
+
     @override
     def part_1(self, filepath: pathlib.Path) -> int | str:
+        intervals: list[list[int]] = []
+        result: int = 0
         with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            pass
+            for start, end in self._get_intervals(file):
+                intervals.append([start, end])
+
+            for line in file:
+                line = line.strip()
+                x = int(line)
+                result += any(start <= x <= end for start, end in intervals)
+
+        return result
 
     @override
     def part_2(self, filepath: pathlib.Path) -> int | str:
+        intervals: list[list[int]] = []
         with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            pass
+            for start, end in self._get_intervals(file):
+                intervals.append([start, end])
+
+        merged_intervals: list[list[int]] = []
+
+        for start, end in sorted(intervals):
+            if merged_intervals and start <= merged_intervals[-1][1]:
+                merged_intervals[-1][1] = max(merged_intervals[-1][1], end)
+            else:
+                merged_intervals.append([start, end])
+
+        return sum(end - start + 1 for start, end in merged_intervals)
