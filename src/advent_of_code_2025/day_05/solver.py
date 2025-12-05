582226d33ff4b21d3b790e70ca404a8bbded71a1
Keith Dao <keith.dao.248@gmail.com>
2025-12-05 19:23:02 +1000

    perf: improve interval creation and search for day 5 solver.

--- src/advent_of_code_2025/day_05/solver.py
+++ src/advent_of_code_2025/day_05/solver.py
@@ -1,12 +1,13 @@
 """Day 5 solver."""
 
+import bisect
 import sys
 from typing import TYPE_CHECKING, override
 
 if TYPE_CHECKING:
     from collections.abc import Generator
-    from io import TextIOWrapper
     import pathlib
+    from typing import TextIO
 
 from .. import base
 
@@ -14,7 +15,7 @@ from .. import base
 class Solver(base.Solver):
     """Day 5 solver."""
 
-    def _get_intervals(self, file: TextIOWrapper) -> Generator[tuple[int, int]]:
+    def _read_intervals(self, file: TextIO) -> Generator[tuple[int, int]]:
         """Yields the intervals in the input.
 
         Args:
@@ -29,34 +30,43 @@ class Solver(base.Solver):
             yield start, end
             line = file.readline().strip()
 
+    def _get_intervals(self, file: TextIO) -> list[tuple[int, int]]:
+        """Gets the intervals within the input.
+
+        Args:
+            file: The opened input file.
+
+        Returns:
+            The intervals with overlapping intervals merged.
+        """
+        unmerged_intervals = sorted(self._read_intervals(file))
+
+        intervals: list[tuple[int, int]] = []
+        for start, end in unmerged_intervals:
+            if intervals and start <= intervals[-1][1]:
+                intervals[-1] = intervals[-1][0], max(intervals[-1][1], end)
+            else:
+                intervals.append((start, end))
+
+        return intervals
+
     @override
     def part_1(self, filepath: pathlib.Path) -> int | str:
-        intervals: list[list[int]] = []
         result: int = 0
         with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            for start, end in self._get_intervals(file):
-                intervals.append([start, end])
+            intervals = self._get_intervals(file)
 
             for line in file:
                 line = line.strip()
                 x = int(line)
-                result += any(start <= x <= end for start, end in intervals)
+                i = bisect.bisect_left(intervals, x, key=lambda x: x[1])
+                result += i < len(intervals) and intervals[i][0] <= x <= intervals[i][1]
 
         return result
 
     @override
     def part_2(self, filepath: pathlib.Path) -> int | str:
-        intervals: list[list[int]] = []
         with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            for start, end in self._get_intervals(file):
-                intervals.append([start, end])
+            intervals = self._get_intervals(file)
 
-        merged_intervals: list[list[int]] = []
-
-        for start, end in sorted(intervals):
-            if merged_intervals and start <= merged_intervals[-1][1]:
-                merged_intervals[-1][1] = max(merged_intervals[-1][1], end)
-            else:
-                merged_intervals.append([start, end])
-
-        return sum(end - start + 1 for start, end in merged_intervals)
+        return sum(end - start + 1 for start, end in intervals)
