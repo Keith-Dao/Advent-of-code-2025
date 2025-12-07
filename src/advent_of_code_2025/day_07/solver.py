8c6c21128f4fc8b32476d745196b56f962d50827
Keith Dao <keith.dao.248@gmail.com>
2025-12-07 17:18:20 +1000

    refactor: extract common logic.

--- src/advent_of_code_2025/day_07/solver.py
+++ src/advent_of_code_2025/day_07/solver.py
@@ -13,44 +13,46 @@ from .. import base
 class Solver(base.Solver):
     """Day 7 solver."""
 
-    @override
-    def part_1(self, filepath: pathlib.Path) -> int | str:
-        result = 0
+    def _generic_solve(self, filepath: pathlib.Path) -> tuple[dict[int, int], int]:
+        """Generic solver for both parts.
+
+        Args:
+            filepath: The input filepath.
+
+        Returns:
+            Tuple with:
+                - The final positions mapped to its count.
+                - The number of splitters used.
+        """
+        start_char = "S"
+        splitter_char = "^"
         with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
             line = file.readline().strip()
-            positions: set[int] = {line.index("S")}
-            for line in file:
-                line = line.strip()
-                new_positions: set[int] = set()
-                for position in positions:
-                    if not 0 <= position < len(line) or line[position] != "^":
-                        new_positions.add(position)
-                        continue
+            positions: dict[int, int] = {line.index(start_char): 1}
+            splits = 0
 
-                    result += 1
-                    new_positions.add(position - 1)
-                    new_positions.add(position + 1)
-
-                positions = new_positions
-
-        return result
-
-    @override
-    def part_2(self, filepath: pathlib.Path) -> int | str:
-        with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            line = file.readline().strip()
-            positions: dict[int, int] = collections.Counter([line.index("S")])
             for line in file:
                 line = line.strip()
                 new_positions: dict[int, int] = collections.Counter()
                 for position, count in positions.items():
-                    if not 0 <= position < len(line) or line[position] != "^":
+                    if not 0 <= position < len(line) or line[position] != splitter_char:
                         new_positions[position] += count
                         continue
 
+                    splits += 1
                     new_positions[position - 1] += count
                     new_positions[position + 1] += count
 
                 positions = new_positions
 
+        return positions, splits
+
+    @override
+    def part_1(self, filepath: pathlib.Path) -> int | str:
+        _, splits = self._generic_solve(filepath)
+        return splits
+
+    @override
+    def part_2(self, filepath: pathlib.Path) -> int | str:
+        positions, _ = self._generic_solve(filepath)
         return sum(positions.values())
