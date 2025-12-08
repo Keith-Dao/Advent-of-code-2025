fa12dba5e29269ed7c21961bb901e2a999bd3719
Keith Dao <keith.dao.248@gmail.com>
2025-12-08 21:57:24 +1000

    feat: add initial day eight solver.

--- src/advent_of_code_2025/day_08/solver.py
+++ src/advent_of_code_2025/day_08/solver.py
@@ -1,5 +1,6 @@
 """Day 8 solver."""
 
+import heapq
 import sys
 from typing import TYPE_CHECKING, override
 
@@ -9,15 +10,80 @@ if TYPE_CHECKING:
 from .. import base
 
 
+class UnionFind:
+    def __init__(self, n: int) -> None:
+        self.parents: list[int] = list(range(n))
+        self.ranks: list[int] = [1] * n
+        self.components: int = n
+
+    def find(self, x: int) -> int:
+        while x != self.parents[x]:
+            self.parents[x] = x = self.parents[self.parents[x]]
+
+        return x
+
+    def union(self, x: int, y: int) -> None:
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:
+            return
+
+        if self.ranks[x] < self.ranks[y]:
+            x, y = y, x
+
+        self.components -= 1
+        self.ranks[x] += self.ranks[y]
+        self.parents[y] = x
+
+
 class Solver(base.Solver):
     """Day 8 solver."""
 
     @override
-    def part_1(self, filepath: pathlib.Path) -> int | str:
+    def part_1(self, filepath: pathlib.Path, pairs: int = 1000) -> int | str:
+        points: list[tuple[int, int, int]] = []
         with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            pass
+            for line in file:
+                x, y, z = (int(x) for x in line.strip().split(","))
+                points.append((x, y, z))
+
+        uf = UnionFind(len(points))
+
+        def distance(i: int, j: int) -> int:
+            return sum((x - y) ** 2 for x, y in zip(points[i], points[j]))
+
+        edges = heapq.nsmallest(
+            pairs,
+            ((distance(i, j), i, j) for i in range(len(points)) for j in range(i)),
+        )
+
+        for _, i, j in edges:
+            uf.union(i, j)
+
+        circuits = {uf.find(i) for i in range(len(points))}
+        a, b, c = heapq.nlargest(3, (uf.ranks[x] for x in circuits))
+        return a * b * c
 
     @override
     def part_2(self, filepath: pathlib.Path) -> int | str:
+        points: list[tuple[int, int, int]] = []
         with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            pass
+            for line in file:
+                x, y, z = (int(x) for x in line.strip().split(","))
+                points.append((x, y, z))
+
+        uf = UnionFind(len(points))
+
+        def distance(i: int, j: int) -> int:
+            return sum((x - y) ** 2 for x, y in zip(points[i], points[j]))
+
+        heap = [(distance(i, j), i, j) for i in range(len(points)) for j in range(i)]
+        heapq.heapify(heap)
+        last = (0, 0)
+
+        while uf.components > 1:
+            _, i, j = heapq.heappop(heap)
+            uf.union(i, j)
+            last = (points[i][0], points[j][0])
+
+        return last[0] * last[1]
