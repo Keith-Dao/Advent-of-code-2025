ebf58919b94347ef73a407ab031cab255ba7ad7f
Keith Dao <keith.dao.248@gmail.com>
2025-12-11 17:00:43 +1000

    feat: add initial day eleven solver.

--- src/advent_of_code_2025/day_11/solver.py
+++ src/advent_of_code_2025/day_11/solver.py
@@ -1,5 +1,6 @@
 """Day 11 solver."""
 
+import functools
 import sys
 from typing import TYPE_CHECKING, override
 
@@ -14,10 +15,52 @@ class Solver(base.Solver):
 
     @override
     def part_1(self, filepath: pathlib.Path) -> int | str:
+        graph: dict[str, list[str]] = {}
         with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            pass
+            for line in file:
+                source, destinations = line.strip().split(": ")
+                graph[source] = []
+                for destination in destinations.split():
+                    graph[source].append(destination)
+
+        result = 0
+        stack = ["you"]
+        while stack:
+            node = stack.pop()
+            if node == "out":
+                result += 1
+                continue
+
+            for next_node in graph[node]:
+                stack.append(next_node)
+
+        return result
 
     @override
     def part_2(self, filepath: pathlib.Path) -> int | str:
+        graph: dict[str, list[str]] = {}
         with open(filepath, "r", encoding=sys.getdefaultencoding()) as file:
-            pass
+            for line in file:
+                source, destinations = line.strip().split(": ")
+                graph[source] = []
+                for destination in destinations.split():
+                    graph[source].append(destination)
+
+        @functools.cache
+        def dfs(node: str, state: int) -> int:
+            if node == "out":
+                return int(state == 0b11)
+
+            result = 0
+            for next_node in graph[node]:
+                next_state = state
+                if next_node == "dac":
+                    next_state |= 1
+                if next_node == "fft":
+                    next_state |= 1 << 1
+
+                result += dfs(next_node, next_state)
+
+            return result
+
+        return dfs("svr", 0)
--- test/test_day_11.py
+++ test/test_day_11.py
@@ -10,4 +10,39 @@ from .base_test import BaseTests
 class TestDay11(BaseTests):
     """Tests the day 11 solver."""
 
-    cases = []
+    cases = [
+        (
+            textwrap.dedent("""\
+                aaa: you hhh
+                you: bbb ccc
+                bbb: ddd eee
+                ccc: ddd eee fff
+                ddd: ggg
+                eee: out
+                fff: out
+                ggg: out
+                hhh: ccc fff iii
+                iii: out"""),
+            5,
+            None,
+        ),
+        (
+            textwrap.dedent("""\
+                svr: aaa bbb
+                aaa: fft
+                fft: ccc
+                bbb: tty
+                tty: ccc
+                ccc: ddd eee
+                ddd: hub
+                hub: fff
+                eee: dac
+                dac: fff
+                fff: ggg hhh
+                ggg: out
+                hhh: out
+                """),
+            None,
+            2,
+        ),
+    ]
